437d1b080140b6412e443fc78829126fb4e25b87
Jeshika Mahaseth <143277188+JeshikaMahaseth@users.noreply.github.com>
2025-02-06 21:28:02 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,30 @@
+<?php include_once "./include/header.php"; ?>
+
+<div class="container">
+    <div class="row">
+        <img src="images/SRM.png" class="mx-auto d-block" alt="Logo" style="width: 55%; margin-top:30px;">
+
+        <div class="container col-8" style="margin-top: 30px;">
+            <div class="card">
+                <div class="card-header text-center">
+                    <strong>SRMIST</strong>
+                </div>
+
+                <div class="card-body">
+
+                <strong class="card-title">DEVELOPER</strong>
+                <ul>
+                    <li>Sahil [RA2111003011856]</li>
+                </ul>
+            </div>
+
+            <div class="card-footer text-center">
+                Project-Guide: Dr. Rama Rajavarman
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include_once "./include/footer.php"; ?>
